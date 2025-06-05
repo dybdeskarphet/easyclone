2f65bf4b2209cbc2339e014941eb817c83ccfbb9
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-06-05 19:03:44 +0300

    feat: Run backup operation concurrently and get rclone args from the config

--- src/main.py
+++ src/main.py
@@ -9,9 +9,10 @@ async def main():
     backup_paths: list[str] = get_config_value(config, "backup", "backup_paths")
     remote_name = get_config_value(config, "backup", "remote_name").rstrip("/\\")
     root_dir = get_config_value(config, "backup", "root_dir").rstrip("/\\")
+    rclone_args = get_config_value(config, "rclone", "args")
     organized_backup_paths = organize_paths(backup_paths, remote_name, root_dir)
 
-    _ = await backup(organized_backup_paths, CommandType.COPY, True)
+    _ = await backup(organized_backup_paths, CommandType.COPY, True, rclone_args)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- src/rclone.py
+++ src/rclone.py
@@ -1,11 +1,15 @@
 import asyncio
+from asyncio.tasks import Task
+import shlex
 
 from enums import BackupLog, CommandType, LogLevel
 from utils import collapse_user, log
 
-async def backup_command(command_type: CommandType, source: str, dest: str, rclone_args: list[str]):
-    cmd = f"rclone {command_type.value} --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s".split()
+async def backup_command(command_type: CommandType, source: str, dest: str, rclone_args: list[str], verbose: bool = False):
+    cmd = f"rclone {command_type.value}".split()
     cmd += [source, dest]
+    cmd += [part for arg in rclone_args for part in shlex.split(str(arg))]
+    log(f"Backing up {collapse_user(source)}", BackupLog.WAIT)
     process = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
 
     stdout, stderr = await process.communicate()
@@ -13,32 +17,28 @@ async def backup_command(command_type: CommandType, source: str, dest: str, rclo
     stdout = stdout.decode(errors="ignore").strip()
     stderr = stderr.decode(errors="ignore").strip()
 
-    if process.returncode is not None:
-        returncode = process.returncode
+    _ = await process.wait()
+
+    if process.returncode == 0:
+        log(f"Backed up successfully: {collapse_user(source)}", BackupLog.OK)
+    elif process.returncode == 1:
+        log(f"Back up operation failed: {collapse_user(source)}", BackupLog.ERR)
     else:
-        returncode = 0
+        log(f"Back up operation failed with {process.returncode} exit code: {collapse_user(source)}", BackupLog.ERR)
 
-    return {
-        "stdout": stdout,
-        "stderr": stderr,
-        "returncode": returncode,
-    }
+    if(verbose):
+        if stderr != "":
+            log(f"{stderr}", LogLevel.WARN)
 
-async def backup(paths: list[dict[str,str]], command_type: CommandType, verbose: bool):
+        if stdout != "":
+            log(f"{stdout}", LogLevel.LOG)
+
+async def backup(paths: list[dict[str,str]], command_type: CommandType, verbose: bool, rclone_args: list[str]):
+    tasks: list[Task[None]] = []
+
     for path in paths:
         source, dest = path["source"], path["dest"]
+        task = asyncio.create_task(backup_command(command_type, source, dest, rclone_args, verbose))
+        tasks.append(task)
 
-        log(f"Backing up {collapse_user(source)}", BackupLog.WAIT)
-        stdout, stderr, returncode = (await backup_command(command_type, source, dest)).values()
-
-        if(returncode == 0):
-            log(f"Backed up successfully: {collapse_user(source)}", BackupLog.OK)
-        else:
-            log(f"Back up operation failed: {collapse_user(source)}", BackupLog.ERR)
-
-        if(verbose):
-            if(returncode != 0):
-                log(f"{stderr}", LogLevel.ERROR)
-                log(f"{stdout}", LogLevel.LOG)
-            else:
-                log(f"{stdout}", LogLevel.LOG)
+    _ = await asyncio.gather(*tasks)
