035f949202fe2129cb0a6d64084dbe6358e5aa44
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2025-06-06 04:34:37 +0300

    docs: Create a to-do for fixing multiple directories on concurrnet backups problem

---
+++ src/rclone/create_folders.py
@@ -0,0 +1,4 @@
+# TODO: Create a create_folders function that creates the folders on the remote before backing up
+# 1. Create a list from the uniquie directory paths.
+# 2. Create a tree from the each directory (use basename for files, nothing is required for dirs)
+# 2. Create directories using BFS concurrently.
