0f809f195e1bdb3d136f65200541b70b643324f0
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-09-11 20:10:28 +0300

    fix(nix): use pkgs.stdenv... instead of pkgs.system

--- nix/module.nix
+++ nix/module.nix
@@ -43,8 +43,8 @@ in
 
     package = lib.mkOption {
       type = lib.types.package;
-      default = self.packages.${pkgs.system}.default;
-      defaultText = lib.literalExpression "self.packages.\${pkgs.system}.default";
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      defaultText = lib.literalExpression "self.packages.\${pkgs.stdenv.hostPlatform.system}.default";
       description = "The easyclone package to install and use.";
     };
 
